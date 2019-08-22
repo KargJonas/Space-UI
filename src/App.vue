c131ee5256cee9b82a9e6eaa8f2879495ae04bfc
Jonas Karg <jonas.karg@hotmail.com>
2019-08-22 16:34:53 +0200

    Misc improvements

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div id="app">
     <Terminal
       title="BOOTED MICROTECH CRD-8 COMBAT ASSAULT DECK :: LINK"
-      main="LOL"
+      v-bind:main="terminalText"
       footer="FOOTER"
       height="100px"
       width="500px"
       x="100px"
       y="50px"
-      speed="100"
+      speed="1"
     />
+
+    <Terminal title="||" width="100px" x="200px" y="500px" />
   </div>
 </template>
 
@@ -17,9 +19,22 @@
 import Terminal from './components/Terminal.vue'
 import "./global.scss";
 
+const terminalText = `SHIELDS ........ 100%
+CHECKS ......... 100%
+SHIELDS ........ 100%
+CHECKS ......... 100%
+SHIELDS ........ 100%
+CHECKS ......... 100%
+SHIELDS ........ 100%
+CHECKS ......... 100%
+SHIELDS ........ 100%
+CHECKS ......... 100%
+`;
+
 export default {
   name: 'app',
   components: { Terminal },
+  data: () => ({ terminalText })
 }
 </script>
 
--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -5,7 +5,7 @@
       <img src="../assets/warning.svg" />
     </div>
     <div v-if="main" class="main" v-bind:style="{ height }">
-      <div class="scroller" ref="scroller">{{mainText}}</div>
+      <pre class="scroller" ref="scroller">{{mainText}}</pre>
     </div>
     <div v-if="footer" class="footer">{{footer}}</div>
   </div>
@@ -26,11 +26,12 @@ export default {
 
       setTimeout(
         () => requestAnimationFrame(() => this.type(index)),
-        this.speed
+        this.speed || 0
       );
     }
   },
   mounted() {
+    if (!this.main) return;
     this.scroller = this.$refs.scroller;
     this.scroller.addEventListener("wheel", e => e.preventDefault());
     this.scroller.addEventListener("touchmove", e => e.preventDefault());
@@ -67,7 +68,7 @@ export default {
     img {
       height: 30px;
       position: absolute;
-      right: 10px;
+      right: 16px;
       top: 50%;
       transform: translateY(-50%);
     }
@@ -80,7 +81,7 @@ export default {
       overflow-y: scroll;
       padding-right: 40px;
       max-height: 100%;
-      margin-right: -100px;
+      margin: 0 -100px 0 0;
       padding-right: 80px;
     }
   }
